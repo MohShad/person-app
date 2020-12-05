9e55c11abf66ac2da21179a59946756c1c4e672c
Mohammad Shadnik <mohammad.shadnik@gmail.com>
2020-12-05 14:05:11 -0300

    Criação do projeto

---
+++ src/main/java/br/com/person/api/restapiperson/RestApiPersonApplication.java
@@ -0,0 +1,13 @@
+package br.com.person.api.restapiperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/person/api/restapiperson/RestApiPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.person.api.restapiperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
